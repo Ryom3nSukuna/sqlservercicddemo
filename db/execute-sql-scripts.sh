b5cf48ac8e8ec1e19544eae2f4322088d7c155f0
Farhan <farhaansays@gmail.com>
2025-01-23 23:02:46 +0530

    44. fixed execute-sql-scripts.sh

--- db/execute-sql-scripts.sh
+++ db/execute-sql-scripts.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Print received variables for debugging
-echo "SPRINT_FOLDER=${SPRINT_FOLDER}"
-echo "DATA-BASE=${DATA-BASE}"
-echo "DB-UID=${DB-UID}"
-echo "DB-PWD=${DB-PWD}"
+echo "ISPRINT_FOLDER=${ISPRINT_FOLDER}"
+echo "IDATABASE=${IDATABASE}"
+echo "IDBUID=${IDBUID}"
+echo "IDBPWD=${IDBPWD}"
 
-echo "Executing SQL scripts in Sprint Folder: ${SPRINT_FOLDER}..."
-echo "Connecting to database ${DATABASE} with user ${DBUID}."
+echo "Executing SQL scripts in Sprint Folder: ${ISPRINT_FOLDER}..."
+echo "Connecting to database ${IDATABASE} with user ${IDBUID}."
 
 # Navigate to the correct directory
-cd /var/opt/sqlserver/db/${SPRINT_FOLDER}/Exec || {
-    echo "Directory /var/opt/sqlserver/db/${SPRINT_FOLDER}/Exec does not exist."
+cd /var/opt/sqlserver/db/"${ISPRINT_FOLDER}"/Exec || {
+    echo "Directory /var/opt/sqlserver/db/${ISPRINT_FOLDER}/Exec does not exist."
     exit 1
 }
 
 # Execute all .sql scripts
 for sql_file in *.sql; do
     echo "Executing ${sql_file}..."
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U "${DB-UID}" -P "${DB-PWD}" -d "${DATA-BASE}" -i "${sql_file}"
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U "${IDBUID}" -P "${IDBPWD}" -d "${IDATABASE}" -i "${sql_file}"
     if [ $? -eq 0 ]; then
         echo "Successfully executed ${sql_file}"
     else
