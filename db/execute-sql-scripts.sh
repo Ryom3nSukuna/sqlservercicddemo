da056d477c8df27972d6d19a698fe96a7524b3b1
Farhan <farhaansays@gmail.com>
2025-01-23 21:58:34 +0530

    36. fixed execute-sql-scripts.sh

--- db/execute-sql-scripts.sh
+++ db/execute-sql-scripts.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# Capture arguments from the command line
-for arg in "$@"
-do
-    eval "$arg"
-done
-
-echo "Executing SQL scripts in Sprint Folder: "$SPRINT_FOLDER"..."
-echo "Connecting to database $DATABASE with user "$DBUID"."
+echo "Executing SQL scripts in Sprint Folder: ${SPRINT_FOLDER}..."
+echo "Connecting to database ${DATABASE} with user ${DBUID}."
 
 # Navigate to the correct directory
-cd /var/opt/sqlserver/db/"$SPRINT_FOLDER"/Exec || exit
+cd /var/opt/sqlserver/db/${SPRINT_FOLDER}/Exec || {
+    echo "Directory /var/opt/sqlserver/db/${SPRINT_FOLDER}/Exec does not exist."
+    exit 1
+}
 
-# Iterate over .sql files and execute
+# Execute all .sql scripts
 for sql_file in *.sql; do
-    echo "Executing $sql_file..."
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U "$DBUID" -P "$DBPWD" -d "$DATABASE" -i "$sql_file"
+    echo "Executing ${sql_file}..."
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U "${DBUID}" -P "${DBPWD}" -d "${DATABASE}" -i "${sql_file}"
     if [ $? -eq 0 ]; then
-        echo "Successfully executed $sql_file"
+        echo "Successfully executed ${sql_file}"
     else
-        echo "Failed to execute $sql_file" >&2
+        echo "Failed to execute ${sql_file}" >&2
         exit 1
     fi
 done
