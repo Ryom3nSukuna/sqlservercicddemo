d8d9ca828da78535de09b1611b66d5da7b09e465
Farhan <farhaansays@gmail.com>
2025-01-24 15:36:04 +0530

    18. Find ENV File inside Container

--- db/execute-sql-scripts.sh
+++ db/execute-sql-scripts.sh
@@ -5,7 +5,7 @@ cd /var/opt/sqlserver/db/${SPRINT_FOLDER}/Exec || exit 1
 
 for file in *.sql; do
   echo "Executing $file..."
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U ${DB_UID} -P ${SA_PASSWORD} -d ${DB_NAME} -i "$file"
+  ACCEPT_EULA=Y /opt/mssql-tools/bin/sqlcmd -S localhost -U ${DB_UID} -P ${SA_PASSWORD} -d ${DB_NAME} -i "$file"
   if [ $? -eq 0 ]; then
     echo "Success: $file"
   else
